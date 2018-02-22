c9a6b8f7d738b2316470363a5d7416b9bf1f4fff
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-02-22 09:10:52 +0100

    New Script: Ability to set Page Size 256

---
+++ BlueCat Address Manager Page Size 256.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        BlueCat Address Manager Page Size 256
+// @namespace   *
+// @description Add option for Page Size of 256 in BlueCat Address Manager
+// @include     */app*
+// @version     1
+// @grant       none
+// @author      Marius Galm
+// @copyright   2018, Marius Galm
+// @license		MIT
+// @icon        https://www.bluecatnetworks.com/wp-content/themes/bluecat/assets/img/icons/favicon.ico
+// ==/UserScript==
+
+if (document.readyState === "interactive" ) {
+    var sel = document.getElementById("PropertySelection");
+    if(sel){
+        var option = document.createElement("option");
+        option.text = "256";
+        option.value = "256";
+        sel.appendChild(option);
+    }
+}
