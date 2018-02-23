1e3f2a11a21f8ff06779d834e27ee30782e90ce9
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-02-23 10:25:38 +0100

    update ver #2
    Added Value to bottom selection box as well

--- BlueCat Address Manager Page Size 256.user.js
+++ BlueCat Address Manager Page Size 256.user.js
@@ -3,7 +3,7 @@
 // @namespace   *
 // @description Add option for Page Size of 256 in BlueCat Address Manager
 // @include     */app*
-// @version     1
+// @version     2
 // @grant       none
 // @author      Marius Galm
 // @copyright   2018, Marius Galm
@@ -18,5 +18,12 @@ if (document.readyState === "interactive" ) {
         option.text = "256";
         option.value = "256";
         sel.appendChild(option);
+    }
+    var sel0 = document.getElementById("PropertySelection_0");
+    if(sel0){
+        var option = document.createElement("option");
+        option.text = "256";
+        option.value = "256";
+        sel0.appendChild(option);
     }
 }
