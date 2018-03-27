c67ccced76fab72507dcea92319096c75cb41e78
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-03-27 09:12:07 +0200

    Added Zone Quick Deploy Button
    Quick Deployment Support from Network AND Zone

--- BlueCat Address Manager Direct Actions_Deploy.user.js
+++ BlueCat Address Manager Direct Actions_Deploy.user.js
@@ -4,7 +4,7 @@
 // @description Add direct buttons for Deploy Actions in BlueCat Address Manager
 // @include     */app*
 // @license		MIT
-// @version     1
+// @version     2
 // @grant       none
 // @copyright   2018, Marius Galm
 // @license		MIT
@@ -29,7 +29,6 @@ if (document.readyState === "interactive" ) {
             z.innerHTML='<span id="deployStatusButton" class="title"><a style="color:inherit; text-decoration: none;" href="/app?component=%24ValueObjectFormTable_14.%24ComboButtonBar.%24ComboButton.direct&page=ConfigurationPage&service=direct&session=T&sp=Spage%3DDeploymentStatus&sp=Spage%3DDeploymentStatus"><img src="/images/icons/small/flash_question.gif" border="0">&nbsp;<span class="title-center"><b style="vertical-align: super;">Deployment Status</b></a></span>';
         }
     }
-
     if (/ Page: IP4NetworkPage /.test(page)) {
         var topBar = document.getElementsByClassName('value-table-topToolBar')[0];
         var tBody = topBar.getElementsByTagName('tbody')[2];
@@ -40,5 +39,14 @@ if (document.readyState === "interactive" ) {
         x.innerHTML='<span id="deployButton" class="title"><a style="color:inherit; text-decoration: none;" href="javascript:remoteSubmitLink( document.getElementById( \'form\' ), \'SQuickDeploy\' );"><img src="/images/icons/small/flash.gif" border="0">&nbsp;<b style="vertical-align: super;">Quick Deploy</b></a></span>';
         var y = tR.insertCell(-1);
     }
-
+    if (/ Page: ZoneDetails /.test(page)) {
+        var topBar = document.getElementsByClassName('value-table-topToolBar')[0];
+        var tBody = topBar.getElementsByTagName('tbody')[2];
+        var tR = tBody.getElementsByTagName('tr')[0];
+        var w = tR.insertCell(-1);
+        w.innerHTML='<div class="separator"></div>';
+        var x = tR.insertCell(-1);
+        x.innerHTML='<span id="deployButton" class="title"><a style="color:inherit; text-decoration: none;" href="javascript:remoteSubmitLink( document.getElementById( \'form\' ), \'SQuickDeploy\' );"><img src="/images/icons/small/flash.gif" border="0">&nbsp;<b style="vertical-align: super;">Quick Deploy</b></a></span>';
+        var y = tR.insertCell(-1);
+    }
 }
