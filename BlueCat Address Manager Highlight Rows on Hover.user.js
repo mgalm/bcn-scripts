9fe18af60b809462681af6b22fe6f2524bfc3f9a
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-04-04 14:44:50 +0200

    New Script
    Highlight row in tables to better visualize the cursor and data

---
+++ BlueCat Address Manager Highlight Rows on Hover.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        BlueCat Address Manager Highlight Rows on Hover
+// @namespace   *
+// @description Highlight the respective table row while hovering over it in BlueCat Address Manager
+// @include     */app*
+// @version     1
+// @grant       none
+// @author      Marius Galm
+// @copyright   2018, Marius Galm
+// @license		MIT
+// @icon        https://www.bluecatnetworks.com/wp-content/themes/bluecat/assets/img/icons/favicon.ico
+// @require     http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+//jQuery fix
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+$("#outerTable tr").not(':first').hover(
+    function () {
+        $(this).css("border","solid thin");
+        $(this).css("border-color","#9FB5C5");
+
+    },
+    function () {
+        $(this).css("border-color","");
+        $(this).css("border","");
+
+    }
+);
