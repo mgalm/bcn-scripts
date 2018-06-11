dd9866a791815cba4c01a11b75f97b7a038aee9d
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-06-11 12:50:52 +0200

    Hover Address Type Title for Images
    Modify all address type images to show the type in text on hover over in BlueCat Address Manager

---
+++ BlueCat Address Manager Hover Address Types.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name        BlueCat Address Manager Hover Address Type
+// @namespace   *
+// @description Modify all address type images to show the type in text on hover over in BlueCat Address Manager
+// @include     */app*
+// @license		MIT
+// @version     1
+// @grant       none
+// @copyright   2018, Marius Galm
+// @icon        https://www.bluecatnetworks.com/wp-content/uploads/2018/03/cropped-bluecat-favicon-32x32.png
+// ==/UserScript==
+
+if (document.readyState === "interactive" ) {
+    var ins = document.querySelectorAll("img");
+
+    for (var i = 0; i < ins.length; i++) {
+        var image = ins[i];
+        if (image !== undefined) {
+            // odd, shouldn't happen
+            if (image.src.indexOf("pawn_glass_red.gif") > -1) {
+
+                image.title = "Network IP /Broadcast";
+            } else if (image.src.indexOf("pawn_glass_yellow.gif") > -1) {
+                image.title = "Gateway";
+            } else if (image.src.indexOf("pawn_glass_blue.gif") > -1) {
+                image.title = "Static";
+            } else if (image.src.indexOf("pawn_glass_green.gif") > -1) {
+                image.title = "Reserved";
+            } else if (image.src.indexOf("pawn_glass_white_new_attention.gif") > -1) {
+                image.title = "DHCP Free";
+            } else if (image.src.indexOf("pawn_glass_blue_new.gif") > -1) {
+                image.title = "DHCP Allocated";
+            } else if (image.src.indexOf("pawn_glass_green_new.gif") > -1) {
+                image.title = "DHCP Reserved";
+            } else if (image.src.indexOf("pawn_glass_white.gif") > -1) {
+                image.title = "Free";
+            } else if (image.src.indexOf("pawn_glass_white_exclude.gif") > -1) {
+                image.title = "DHCP Exclude";
+            } else if (image.src.indexOf("pawn_glass_white_new.gif") > -1) {
+                image.title = "DHCP Range";
+            }
+        }
+        //console.log(ins[i]);
+    }
+}
