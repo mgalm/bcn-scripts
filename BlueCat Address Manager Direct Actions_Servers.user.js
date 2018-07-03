42307369b9efcf68d4325e4a9448b40cf5882f0b
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-07-03 17:58:08 +0200

    Update for Server Widget (MyIPAM)
    Now also adds the buttons to the Server Widgets in the MyIPAM Homepage

--- BlueCat Address Manager Direct Actions_Servers.user.js
+++ BlueCat Address Manager Direct Actions_Servers.user.js
@@ -4,16 +4,88 @@
 // @description Add direct buttons for Server Actions in BlueCat Address Manager
 // @include     */app*
 // @license		MIT
-// @version     6
+// @version     7
 // @grant       none
 // @copyright   2018, Marius Galm
 // @license		MIT
 // @icon        https://www.bluecatnetworks.com/wp-content/uploads/2018/03/cropped-bluecat-favicon-32x32.png
 // ==/UserScript==
 
+function isNodeList(nodes) {
+    var stringRepr = Object.prototype.toString.call(nodes);
+
+    return typeof nodes === 'object' &&
+        /^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
+        (typeof nodes.length === 'number') &&
+        (nodes.length > 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
+}
+
+// give up after 15 tries = 7,5 sec
+var k = 0, howManyTimes = 15;
+function getServers() {
+    var count = k+1;
+    console.log(" + looking for widgets "+count+"/"+howManyTimes);
+    var servers = document.querySelectorAll("div[id^='gwt-uid']");
+    k++;
+    if(( k < howManyTimes )&&(isNodeList(servers)==false)){
+        setTimeout( getServers, 500 );
+    }
+    else if ((servers!==undefined)&&(servers.length>0)) {
+        //console.log(servers);
+        console.log(" => Found Server widgets, adding buttons");
+        for (index = 0; index < servers.length; ++index) {
+            var namecell = servers[index];
+            //console.log(namecell);
+            var isCluster = false;
+            var image = namecell.getElementsByTagName('img');
+            if (image !== undefined) {
+                // odd, shouldn't happen
+                if (image[0].src.indexOf("cluster") > -1) {
+                    isCluster = true;
+                    //console.log(cluster);
+                }
+                var link = namecell.getElementsByTagName("a")[0].href;
+                var rest = link.split("SingleServer:")[1];
+                var server_id = rest.split(":")[0];
+                // add links with icons to namecell
+                var td = namecell.getElementsByTagName("a")[0].parentNode;
+                var tr = td.parentNode;
+                // console.log(" + Adding Buttons")
+                // edit button
+                var x = tr.insertCell(-1);
+                var editlink = "";
+                if (isCluster) {
+                    // special Link for clusters (why? no idea -> the other link let's you set the hostname :-D )
+                    editlink = "/app?component=%24TabbedEntityContainer.%24PagePanel.pageMenu.direct&page=ServerPage&service=direct&session=T&sp=Spage%3DAddEditXHA&sp=Svalue%3DSingleServer%3A"+server_id+"%3A18&sp=ScontextId%3Ddetails&sp=SformMode%3Dedit";
+                }else {
+                    editlink = "/app?component=%24TabbedEntityContainer.%24PagePanel.pageMenu.direct&page=ServerPage&service=direct&session=T&sp=Spage%3DAddEditServer&sp=Svalue%3DSingleServer%3A"+server_id+"%3A18&sp=ScontextId%3Ddetails&sp=SformMode%3Dedit";
+                }
+                // more room between the name and the buttons
+                x.innerHTML='<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="'+editlink+'"><img title="Edit Server Settings" src="/images/icons/small/document_edit.gif" border="0"></a></td>';
+                // view logs button
+                var y = tr.insertCell(-1);
+                var loglink = "/app?component=%24TabbedEntityContainer.%24PagePanel.pageMenu.direct&page=ServerPage&service=direct&session=T&sp=Spage%3DViewLogs&sp=Svalue%3DSingleServer%3A"+server_id+"%3A18"
+                y.innerHTML='<td><a href="'+loglink+'"><img title="View Server Logs" src="/images/icons/small/server_view.gif" border="0"></a></td>';
+                // service config button
+                var z = tr.insertCell(-1);
+                var conflink = "/app?component=%24TabbedEntityContainer.%24PagePanel.pageMenu.direct&page=ServerPage&service=direct&session=T&sp=Spage%3DServerServiceConfigure&sp=Svalue%3DSingleServer%3A"+server_id+"%3A18"
+                z.innerHTML='<td><a href="'+conflink+'"><img title="Service Configuration"  src="/images/icons/small/server_configuration.gif" border="0"></a></td>';
+            }
+        }
+    } else {
+        console.log(" => No Server widgets found, giving up!");
+    }
+};
+
+
 if (document.readyState === "interactive" ) {
     var page = document.childNodes[2].nodeValue;
-    if (/ Page: ConfigurationPage /.test(page)) {
+    if (/ Page: Home /.test(page)) {
+        console.log("Found MyIPAM - Special handling because Widgets loading takes a while...");
+        var index;
+        // loop or something
+        getServers();
+    } else if (/ Page: ConfigurationPage /.test(page)) {
         var subtab = document.getElementsByClassName("TabPanelLabelActive")[0];
         if (/Servers/.test(subtab.innerHTML.trim())) {
             // go through the h
@@ -71,7 +143,7 @@ if (document.readyState === "interactive" ) {
                             //console.log(cluster);
                         } else {
                             var server = 'Found Server "'+name+'" as Managed: "'+managed+'" with Profile "'+profile+'"';
-                           // console.log(server);
+                            // console.log(server);
                         }
                         // only for managed BDDS
                         if (managed === "Yes") {
@@ -111,4 +183,4 @@ if (document.readyState === "interactive" ) {
             }
         }
     }
-}
+} 
