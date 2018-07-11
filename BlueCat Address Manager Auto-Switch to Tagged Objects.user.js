b702e9ecb1a87dcedee61e382ab5327af5cc8492
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-07-11 12:56:20 +0200

    now also works when tag hierarchy in tree view
    prior the script only checked for the emtpy-table class, now will check for treeNode and switch to Tagged Object if none exits. Needs a short timeout though, because the list is dynamically generated (currently 5 x 500ms = 2.5sec)

--- BlueCat Address Manager Auto-Switch to Tagged Objects.user.js
+++ BlueCat Address Manager Auto-Switch to Tagged Objects.user.js
@@ -5,19 +5,49 @@
 // @include     */app*
 // @exclude     */app*sp=ScontextId%3Dtag
 // @license		MIT
-// @version     2
+// @version     3
 // @grant       none
 // @copyright   2018, Marius Galm
 // @license		MIT
 // @icon        https://www.bluecatnetworks.com/wp-content/uploads/2018/03/cropped-bluecat-favicon-32x32.png
 // ==/UserScript==
 
+
+// give up after 5 tries = 2,5 sec
+var k = 0, howManyTimes = 5;
+function getNodes() {
+    var count = k+1;
+    console.log(" + looking for nodes "+count+"/"+howManyTimes);
+    var nodes = document.getElementsByClassName("TreeNode").length;
+    k++;
+    if(( k < howManyTimes )&&(nodes === 0)){
+        setTimeout( getNodes, 500 );
+    }
+    else if ((k === howManyTimes)&&(nodes === 0)) {
+        document.getElementById("contextTabLink_1").click();
+    } else {
+        //console.log("nothing to do");
+        return;
+    }
+};
+
+
 // Exclude Pattern will prevent endless loop :-)
 if (document.readyState === "interactive" ) {
     var page = document.childNodes[2].nodeValue;
     if (/ Page: Tags /.test(page)) {
-    	if (document.getElementsByClassName("empty-table").length === 1) {
-    		document.getElementById("contextTabLink_1").click();
-    	}
+        // check subtab, should only be executed on Tags not Tagged Objects
+        var subtab = document.getElementsByClassName("TabPanelLabelActive")[0];
+        if (subtab.innerText === "Tags") {
+            // Table View
+            if (document.getElementsByClassName("empty-table").length === 1) {
+                document.getElementById("contextTabLink_1").click();
+                // Tree View
+            } else if (document.getElementsByClassName("TreeNode").length === 0) {
+                getNodes();
+                console.log(document.getElementsByClassName("TreeNode").length);
+                //document.getElementById("contextTabLink_1").click();
+            }
+        }
     }
 }
