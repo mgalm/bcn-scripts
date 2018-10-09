c9f8dd74c750c44be95195f2c88abdeb6092073c
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-10-09 15:49:49 +0200

    adjustment for multiple vs single containers

--- BlueCat Address Manager Table Counter.user.js
+++ BlueCat Address Manager Table Counter.user.js
@@ -12,16 +12,74 @@
 // ==/UserScript==
 
 if (document.readyState === "interactive" ) {
-	var outertable = document.getElementById("outerTable");
-	var count = outertable.rows.length - 1;
-	var bottomBars = document.getElementsByClassName('value-table-bottomToolBar');
-	if (( bottomBars !== undefined ) || (bottomBars.length <= 0)) {
-		var bottomBar = bottomBars[0];
-		if ( bottomBar !== undefined ) {
-			var tBody = bottomBar.getElementsByTagName('tbody')[0];
-			var tR = tBody.getElementsByTagName('tr')[0];
-			var w = tR.insertCell(0);
-			w.innerHTML='<span class="title-left"><strong>Count: '+count+'</stong></span>';
-		}
-	}
+    //for dialog-hd
+    var dialogBars = document.getElementsByClassName('dialog-hd');
+    if (( dialogBars !== undefined ) || (dialogBars.length < 0)) {
+        //console.log(dialogBars.length);
+        if (dialogBars.length === 1) {
+            if ( dialogBars[0] !== undefined ) {
+                var idname = dialogBars[0].id;
+                var prefix = idname.split('-bar')[0];
+                var namepart = prefix.split('_');
+                var part = namepart[1];
+                var name = namepart[0];
+                var outertable;
+                if (part === undefined) {
+                    //console.log("iterating through first "+name);
+                    outertable = document.getElementById("outerTable");
+                } else {
+                    part = "_"+part;
+                    //console.log("iterating "+name+" with part "+part);
+                    outertable = document.getElementById("outerTable"+part);
+                    //console.log(outertable);
+                    if ((outertable == null)||(outertable === undefined)) {
+                        // fallback to no part query
+                        //console.log("ahhh");
+                        outertable = document.getElementById("outerTable");
+                        //console.log(outertable);
+                    }
+                }
+                if (outertable.getElementsByClassName("empty-table").length !== 1) {
+                    var count = outertable.rows.length - 1;
+                    var tBody = dialogBars[0].getElementsByTagName('tbody')[0];
+                    console.log(tBody);
+                    var tR = tBody.getElementsByTagName('tr')[0];
+                    console.log(tR);
+                    var w = tR.insertCell(1);
+                    w.innerHTML='<div class="separator"></div>';
+                    var x = tR.insertCell(2);
+                    x.innerHTML='<span class="label">Count: '+count+'</span>';
+                }
+            }
+        } else {
+            for (var i = 0; i < dialogBars.length; i++) {
+                //console.log(dialogBars[i]);
+                if ( dialogBars[i] !== undefined ) {
+                    var idname = dialogBars[i].id;
+                    var prefix = idname.split('-bar')[0];
+                    var namepart = prefix.split('_');
+                    var part = namepart[1];
+                    var name = namepart[0];
+                    var outertable;
+                    if (part === undefined) {
+                        console.log("iterating through first "+name);
+                        outertable = document.getElementById("outerTable");
+                    } else {
+                        part = "_"+part;
+                        console.log("iterating "+name+" with part "+part);
+                        outertable = document.getElementById("outerTable"+part);
+                    }
+                    if (outertable.getElementsByClassName("empty-table").length !== 1) {
+                        var count = outertable.rows.length - 1;
+                        var tBody = dialogBars[i].getElementsByTagName('tbody')[0];
+                        var tR = tBody.getElementsByTagName('tr')[1];
+                        var w = tR.insertCell(-1);
+                        w.innerHTML='<div class="separator"></div>';
+                        var x = tR.insertCell(-1);
+                        x.innerHTML='<span class="label">Count: '+count+'</span>';
+                    }
+                }
+            }
+        }
+    }
 }
