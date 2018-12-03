54da71fdc7db9cd6902110a96ba3edb36cc4c8d5
n3kgalm <32574456+n3kgalm@users.noreply.github.com>
2018-12-03 11:15:22 +0100

    Fix Table layout changing

--- BlueCat Address Manager Restore Button.user.js
+++ BlueCat Address Manager Restore Button.user.js
@@ -3,7 +3,7 @@
 // @description Additional Restore Button in Transaction Details in BlueCat Address Manager
 // @namespace   *
 // @include     */app*page=UndeleteListPage*
-// @version     6
+// @version     7
 // @grant       none
 // @author      Marius Galm
 // @copyright   2018, Marius Galm
@@ -23,8 +23,18 @@ if (document.readyState === "interactive" ) {
 }
 
 function addButton() {
-    if (document.getElementById('Any_15')) {
-        var idVar = document.getElementById("Any_15").innerHTML.trim();
-        document.getElementById("Any_15").innerHTML = idVar+'<p><a id="undeleteLink" name="undelete_hist_History:'+idVar+':0" href="/app?component=undeleteLink&amp;page=UndeleteListPage&amp;service=direct&amp;session=T&amp;sp=SformMode%3Dedit&amp;sp=StableClass%3Dcom.bluecatnetworks.proteus.data.History&amp;sp=StablePageName%3DUndeleteListPage&amp;sp=StableId%3DuserUndeleteList&amp;sp=Spage%3DUndeleteListPage&amp;sp=SextraOptionalValue%3DundeleteType%7Cnull%3BpageBeforeList%7Cnull&amp;sp=Suserspecific%3Dtrue&amp;sp=ShistoryId%3DHistory%3A'+idVar+'%3A0"><img src="/images/icons/small/undelete.gif" border="0"></a></p>';
+    var pane = document.getElementsByClassName("detailsPane");
+    if ((pane !== undefined)||(pane != null)) {
+        var tds = pane[0].getElementsByClassName("detailsPane-fieldName");
+        if ((tds !== undefined)||(tds != null)) {
+            var tdList = Array.prototype.slice.call(tds);
+            tdList.forEach(function(element) {
+                if (element.innerText.trim() == "Transaction Number:") {
+                    var num = parseInt(element.id.split("_")[1])+1;
+                    var idVar = document.getElementById("Any_"+num).innerHTML.trim();
+                    document.getElementById("Any_15").innerHTML = idVar+'<p><a id="undeleteLink" name="undelete_hist_History:'+idVar+':0" href="/app?component=undeleteLink&amp;page=UndeleteListPage&amp;service=direct&amp;session=T&amp;sp=SformMode%3Dedit&amp;sp=StableClass%3Dcom.bluecatnetworks.proteus.data.History&amp;sp=StablePageName%3DUndeleteListPage&amp;sp=StableId%3DuserUndeleteList&amp;sp=Spage%3DUndeleteListPage&amp;sp=SextraOptionalValue%3DundeleteType%7Cnull%3BpageBeforeList%7Cnull&amp;sp=Suserspecific%3Dtrue&amp;sp=ShistoryId%3DHistory%3A'+idVar+'%3A0"><img src="/images/icons/small/undelete.gif" border="0"></a></p>';
+                }
+            });
+        }
     }
 }
